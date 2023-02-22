cc277728b9ed4b759d91346e35f52b2544e1178d
mdturin <matkhan01@yahoo.com>
2023-02-22 16:43:32 +0600

    User model created

---
+++ JwtWebApiTutorial/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtWebApiTutorial
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+    }
+}
