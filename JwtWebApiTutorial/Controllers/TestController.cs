a336051ba6eac7cd8db44baaf97368a5d1e7e44b
Md Turin <matkhan01@yahoo.com>
2023-02-28 01:33:39 +0600

    Jwt web api updated

---
+++ JwtWebApiTutorial/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JwtWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet, Authorize]
+        public ActionResult<List<string>> Get()
+        {
+            List<string> names = new List<string>() { "Md", "Turin" };
+            return Ok(names);
+        }
+    }
+}
+
